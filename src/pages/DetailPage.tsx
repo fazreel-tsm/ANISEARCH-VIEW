dea55e41fb0e9bdd557770493a11044cbb825b48
fazreel-tsm <fazreel.yusman@tech-store.com.my>
2025-10-31 11:24:23 +0800

    added loading screen as component

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -11,6 +11,7 @@ import OverviewTab from "../components/tabs/OverviewTab";
 import CharactersTab from "../components/tabs/CharactersTab";
 import StaffTab from "../components/tabs/StaffTab";
 import { TabButton } from "../components/ui/TabButton";
+import { LoadingScreen } from "../components/LoadingScreen";
 
 export default function DetailPage() {
   const { idSlug } = useParams();
@@ -52,47 +53,7 @@ export default function DetailPage() {
     }
   }, [anime]);
 
-  if (loading)
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen text-center text-text font-headline">
-        {/* Biking Character */}
-        <div className="relative w-full max-w-md h-8 mb-8">
-          <img
-            src="/photos/cycling1.png"
-            alt="Onoda biking"
-            className="absolute left-0 top-[-50px] w-14 animate-bike select-none"
-          />
-          {/* Progress Bar Background */}
-          <div className="w-full h-2 bg-border rounded-full overflow-hidden">
-            {/* Animated Bar */}
-            <div className="h-full bg-header animate-progress" />
-          </div>
-        </div>
-
-        {/* Tailwind keyframes (using inline style since it’s local) */}
-        <style>
-          {`
-          @keyframes progressMove {
-            0% { transform: translateX(-100%); }
-            100% { transform: translateX(100%); }
-          }
-          @keyframes bikeMove {
-            0% { left: 0%; transform: rotate(0deg); opacity: 0; }
-            5% { opacity: 1;}
-            40% { left: 40%; transform: rotate(-10deg); }
-            95% { opacity: 1;}
-            100% { left: 90%; transform: rotate(0deg); opacity: 0; }
-          }
-          .animate-progress {
-            animation: progressMove 1s linear infinite;
-          }
-          .animate-bike {
-            animation: bikeMove 2s linear infinite;
-          }
-        `}
-        </style>
-      </div>
-    );
+  if (loading) return <LoadingScreen />;
 
   if (error?.includes("404")) {
     return <NotFoundPage />;
